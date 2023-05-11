74f33b2e2be31efe30adeed99eca25dd932117e2
AmeliaVegaa <133181467+AmeliaVegaa@users.noreply.github.com>
2023-05-11 09:28:12 +0700

    Create Fighter.java

---
+++ Fighter.java
@@ -0,0 +1,9 @@
+public class Fighter extends Hero {
+    public void attack() {
+        System.out.println("Fighter menyerang dengan meninju!");
+    }
+    
+    public void move() {
+        System.out.println("Fighter bergerak!");
+    }
+}
