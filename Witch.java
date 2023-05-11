1cdcb28e2cb7393d07db64ae095da26465962171
AmeliaVegaa <133181467+AmeliaVegaa@users.noreply.github.com>
2023-05-11 09:29:15 +0700

    Create Witch.java

---
+++ Witch.java
@@ -0,0 +1,9 @@
+public class Witch extends Enemy {
+    public void attack() {
+        System.out.println("Witch menggunakan mantranya!");
+    }
+    
+    public void move() {
+        System.out.println("Witch bergerak!");
+    }
+}
