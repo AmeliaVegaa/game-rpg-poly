0368c28cc53ee0fedb471101c8511054949abc6f
AmeliaVegaa <133181467+AmeliaVegaa@users.noreply.github.com>
2023-05-11 09:26:37 +0700

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,9 @@
+public class Character {
+    public void attack() {
+        System.out.println("Character menyerang!");
+    }
+    
+    public void move() {
+        System.out.println("Character bergerak!");
+    }
+}
